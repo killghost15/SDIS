d96902a53e4b6d8c83139f3ff58eea1295877cb0
killghost15 <killghost15@gmail.com>
2017-02-14 17:50:29 +0000

    hashtable created lookup working with it

--- SDIS-Lab1/src/sdislab1/ServerThread.java
+++ SDIS-Lab1/src/sdislab1/ServerThread.java
@@ -8,7 +8,6 @@ import java.util.Hashtable;
 
 public class ServerThread extends Thread {
 	
-	protected String[] plates = {"Fulano","AA|44|BB"};
 	protected Hashtable<String, String> database = new Hashtable<String,String>();
 	protected DatagramSocket socket = null;
 
@@ -18,8 +17,8 @@ public class ServerThread extends Thread {
 
 	public ServerThread(String name) throws IOException {
 	    super(name);
-	    database.put("Fulano", "AA|44|BB");
-	    database.put("Tipo2", "AB|33|CC");
+	    database.put( "AA|44|BB","Fulano");
+	    database.put("AB|33|CC","Tipo2");
 	    socket = new DatagramSocket(4445);
 	    
 	    byte[] buf = new byte[256];
@@ -36,10 +35,11 @@ public class ServerThread extends Thread {
 	    String answer = "blabla";
 	    
 	    if(splitted[0].equals("lookup")) {
-	    	if(database.contains(splitted[1]))
-	    		answer = "Plate "+splitted[1]+" exists!";
+	    	if(database.containsKey(splitted[1]))
+	    		answer = "Plate "+splitted[1]+" exists!\n"
+	    				+"owner: " + database.get(splitted[1]).toString();
 	    	else
-	    		answer = "Plate "+splitted[1]+" not found!";
+	    		answer = "Plate "+splitted[1]+"NOT_FOUND";
 	    }
 	    
 	    buf = answer.getBytes();
