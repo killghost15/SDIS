35780fe51e96d0fd0bca1b052a3ec5c674fd7657
killghost15 <killghost15@gmail.com>
2017-03-07 14:38:40 +0000

    starting peer connection will start in UDP if i don't understand RMI

---
+++ Project/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		if(args[0].equals("BACKUP"))
+			return;
+			//BACKUP
+		if(args[0].equals("RESTORE"))
+				return;
+				//RESTORE 
+		if(args[0].equals("DELETE"))
+			return;
+			//DELETE
+		if(args[0].equals("STATE"))
+			return;
+			//Space allocation management
+		if(args.length==0)
+			return;
+			//PEER MAC group process
+
+	}
+
+}
---
+++ Project/src/Peer.java
@@ -0,0 +1,17 @@
+
+public class Peer {
+	public int id;
+	public Peer(int id){
+		this.id=id;
+	}
+	public void Backup(){
+		
+	}
+	public void Delete(){
+		
+	}
+	public void Restore(){
+		
+	}
+
+}
