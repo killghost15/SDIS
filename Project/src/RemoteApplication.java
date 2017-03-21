b07efa3e208b25ee2b726735e73150128a8f9b6a
killghost15 <killghost15@gmail.com>
2017-03-21 12:47:48 +0000

    backup first implementation

--- Project/src/RemoteApplication.java
+++ Project/src/RemoteApplication.java
@@ -1,35 +1,97 @@
-import java.rmi.RemoteException;
-import java.util.Hashtable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class RemoteApplication implements RemoteInterface{
 	protected String [] SenderIds;
 	public RemoteApplication(){
-		
+
 	}
 	@Override
-	public String BackupProtocol()  {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public String BackupProtocol(String filename,String Chunkname)  {
+		File inputFile = new File(filename);
 
-	@Override
-	public String RestoreProtocol()  {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		FileInputStream inputStream;
 
-	@Override
-	public String DeleteProtocol()  {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		String newFileName;
 
-	@Override
-	public String ReclaimProtocol()  {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+		FileOutputStream filePart;
 
-}
+		int fileSize = (int) inputFile.length();
+
+		 int nChunks = 0, read = 0, readLength = 64;
+
+		byte[] byteChunkPart;
+
+		try {
+
+			inputStream = new FileInputStream(inputFile);
+
+			while (fileSize > 0) {
+
+				if (fileSize <=64 ) {
+
+					readLength = fileSize;
+
+				}
+
+				byteChunkPart = new byte[readLength];
+
+				read = inputStream.read(byteChunkPart, 0, readLength);
+
+				fileSize -= read;
+
+				assert (read == byteChunkPart.length);
+
+				nChunks++;
+
+				newFileName =Chunkname+"@"+nChunks;
+
+				                       
+
+				filePart = new FileOutputStream(new File(newFileName));
+
+				filePart.write(byteChunkPart);
+
+				filePart.flush();
+
+				filePart.close();
+
+				byteChunkPart = null;
+
+				filePart = null;
+
+			}
+				inputStream.close();
+
+			} catch (IOException exception) {
+
+				exception.printStackTrace();
+
+			}
+
+			return null;
+		}
+
+		@Override
+		public String RestoreProtocol()  {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String DeleteProtocol()  {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String ReclaimProtocol()  {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+
+	}
--- Project/src/RemoteInterface.java
+++ Project/src/RemoteInterface.java
@@ -3,9 +3,10 @@ import java.rmi.RemoteException;
 
 
 public interface RemoteInterface extends Remote{
-	public String BackupProtocol()throws RemoteException;
+	
 	public String RestoreProtocol()throws RemoteException;
 	public String DeleteProtocol()throws RemoteException;
 	public String ReclaimProtocol()throws RemoteException;
+	public String BackupProtocol(String filename, String Chunkname)throws RemoteException;
 }
 
