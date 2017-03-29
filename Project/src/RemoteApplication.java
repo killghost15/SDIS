22a424e82cb8f86cdf74bf9c58385ed9935c8f14
killghost15 <killghost15@gmail.com>
2017-03-29 14:20:53 +0100

    create file .metadata to store sender id and fileId bcs when you encrypt you can't decrypt and encrypting the same name doesn't return the same fileid

--- Project/src/RemoteApplication.java
+++ Project/src/RemoteApplication.java
@@ -7,18 +7,29 @@ import java.io.IOException;
 
 public class RemoteApplication implements RemoteInterface{
 	protected String [] SenderIds;
+	String metadatafile=".metadata";
 	public RemoteApplication(){
-
+		try {
+			FileOutputStream file=new FileOutputStream(".metadata", true);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	@Override
 	public String StoreBackupProtocol(String filename,int Chunknr,byte[]content)  {
 		String answer;		
 		FileOutputStream filePart;
+		
 				try {
 					filePart = new FileOutputStream(new File(filename+".part"+Chunknr));
 					filePart.write(content);
 					filePart.flush();
 					filePart.close();
+					FileOutputStream file=new FileOutputStream(metadatafile,true);
+					file.write(filename.getBytes());
+					file.flush();
+					file.close();
 					answer="Stored";
 				} catch (IOException exception) {
 
