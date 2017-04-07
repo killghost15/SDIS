d14d50033a0b4da55b1f38501bf5095bb024e0d1
killghost15 <killghost15@gmail.com>
2017-04-07 20:44:44 +0100

    sorting out the message in restore

--- Project/src/Peer.java
+++ Project/src/Peer.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
@@ -12,38 +14,39 @@ import java.rmi.server.UnicastRemoteObject;
 
 
 public class Peer {
-	
+
 	private static String mac="224.0.0.3";
 	/* #TODO aldrabei para string pk perdi a paciencia
 	private static byte[] CR ={'0','x','D'};
 	private static byte[] LF={'0','x','A'};
-	*/
+	 */
 	private static String CR="0xD";
 	private static String LF="0xA";	public Peer(){
-		
+
 
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException, NotBoundException{
 		//Estabilish connection with Mac group
 		MulticastSocket socket = new MulticastSocket(Integer.parseInt(args[0]));
+		
 		//socket.setTimeToLive(1);
 		//host_name
-        
-        String []splittedHead;
-       
+
+		String []splittedHead;
+
 		byte[] buf = new byte[64000];
 		InetAddress address = InetAddress.getByName(mac);
-		
+
 		RemoteApplication app=new RemoteApplication();
 		RemoteInterface stub=null;
 		Registry registry=null;
-		
+
 		stub=(RemoteInterface)UnicastRemoteObject.exportObject(app, 0);
 		registry= LocateRegistry.getRegistry();
-		
+
 		DatagramPacket packetreceive = null;
 		DatagramPacket packetsend = null;
-		
+
 		//junta-se ao grupo com o mac
 		socket.joinGroup(address);
 		String msghead=null;
@@ -57,54 +60,98 @@ public class Peer {
 		}
 		//Treat the message received loop infinito
 		//need a break condition that is usefull
-		
+
 		while(true){
-		buf = new byte[stub.getreadLength()];
-		packetreceive = new DatagramPacket(buf, buf.length);
-		socket.receive(packetreceive);
-		String request = new String(packetreceive.getData(), 0, packetreceive.getLength());
-		
-		/*System.out.println(CR+LF);
+			buf = new byte[stub.getreadLength()];
+			packetreceive = new DatagramPacket(buf, buf.length);
+			socket.receive(packetreceive);
+			String request = new String(packetreceive.getData(), 0, packetreceive.getLength());
+
+			/*System.out.println(CR+LF);
 		byte b[]={0xD,0xA};
 		System.out.println(new String(b.toString()));
 		System.out.println(new String(b).getBytes());
-		*/
-		//Regex this way you can put as much spaces as you want it will still divide it the same
-		 msghead=request.split(" +"+CR+LF+" +")[0];
-		 
-		 System.out.println(msghead);
-		
-		
-		splittedHead=msghead.split(" +");
-		//need to convert the "PUTCHUNK" Bytes to String or find away to split the bytes data
-		if(splittedHead[0].equals("PUTCHUNK")){
-			msgbody=request.split(" +"+CR+LF+" +")[1];
-			buf=new byte[256];
-			
-	        System.out.println("Saved file");
-	        answer="STORED "+" "+splittedHead[1]+" "+splittedHead[2]+" " +splittedHead[3]+" " + splittedHead[4]+" " +CR+LF+" ";
-	        buf=answer.getBytes();
-	        
-	        packetsend=new DatagramPacket(buf, buf.length,packetreceive.getAddress(),packetreceive.getPort());
-			socket.send(packetsend);
-			
-			registry.rebind("Teste2", stub);
-			
-			
+			 */
+			//Regex this way you can put as much spaces as you want it will still divide it the same
+			msghead=request.split(" +"+CR+LF+" +")[0];
+
+			System.out.println(msghead);
+
+
+			splittedHead=msghead.split(" +");
+			//need to convert the "PUTCHUNK" Bytes to String or find away to split the bytes data
+			if(splittedHead[0].equals("PUTCHUNK")){
+				msgbody=request.split(" +"+CR+LF+" +")[1];
+				buf=new byte[256];
+
+				System.out.println("Saved file");
+				answer="STORED "+" "+splittedHead[1]+" "+splittedHead[2]+" " +splittedHead[3]+" " + splittedHead[4]+" " +CR+LF+" ";
+				buf=answer.getBytes();
+
+				packetsend=new DatagramPacket(buf, buf.length,packetreceive.getAddress(),packetreceive.getPort());
+				socket.send(packetsend);
+
+				registry.rebind("Teste2", stub);
+
+
+			}
+			if(splittedHead[0].equals("GETCHUNK")){
+
+				File folder = new File(System.getProperty("user.dir"));
+				for(int i=0;i<folder.listFiles().length;i++){
+
+					if(folder.listFiles()[i].getName().split(".part")[0].equals(splittedHead[3])){
+						System.out.println(splittedHead[3]);
+
+						buf=ReadFile(folder.listFiles()[i].getName());
+
+						packetsend=new DatagramPacket(buf, buf.length,packetreceive.getAddress(),packetreceive.getPort());
+						socket.send(packetsend);
+					}
+				}
+
+
+
+			}
+
+
+
 		}
-		if(splittedHead[0].equals("GETCHUNK")){
-			System.getProperty("user.dir");
-			System.out.println(System.getProperty("user.dir"));
-			//File folder = new File(System.getProperty("user.dir"));
-			
-			
-		}
-		
-		
-		
-	}
 		//socket.close();
 	}
 
+	public static byte[] ReadFile(String filename){
+
+		byte[] byteChunkPart;
+		File inputFile = new File(filename);
+
+		FileInputStream inputStream;
+		
+		int fileSize = (int) inputFile.length();
+		byteChunkPart = new byte[fileSize];
+		int nChunks = 0, read = 0;
+
+
+		try {
+			inputStream = new FileInputStream(inputFile);
+
+
+			
+
+			read = inputStream.read(byteChunkPart, 0, fileSize);
+
+			fileSize -= read;
+
+			assert (read == byteChunkPart.length);
+			inputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+
+
+return byteChunkPart;
+	}
 
 }
